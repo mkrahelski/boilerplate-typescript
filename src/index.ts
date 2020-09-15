8166c2b1e6df578858c4098866604d936a109eef
Mikita Krahelski <nikita.krahelski@gmail.com>
2020-09-14 18:23:29 -0700

    Added lint-and-fix, and an extra ignore and a few others probably.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("Hello World");
+function helloWorld( message:string ){
+ 
+    console.log("Hello World");
+    
+    var updatedMessage;
+    
+    return 0;
+    
+   }
+    
+   helloWorld("goodbye world");
+
+
+//it lights up, it needs a string.
+//Type script feeding us info.
+//It doesn't use goodbye, and returns 0.
+//it'll compile fine.
+
+//no variable declations using var?
+
+
+
+//Linting is an inspection tool. THat solves run time errors.
+
+
+//by default litn flags var as an error.
